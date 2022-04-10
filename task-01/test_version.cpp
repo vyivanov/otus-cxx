c7e7b7cdbeb62611f5596034bc67b8fb469d207c
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2022-04-10 06:32:16 +0300

    Change version handling

--- task-01/test_version.cpp
+++ task-01/test_version.cpp
@@ -7,7 +7,7 @@
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
-    BOOST_CHECK(version() > 0);
+    BOOST_TEST(version() != "0.0.0");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
