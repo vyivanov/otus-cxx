ff2742093c8ca8f63257fd828086fabc25150b73
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2022-10-30 16:45:34 +0300

    Add "StubItemReader" implementation

--- task-08/cxx/core/inc/item-reader/stub-item-reader.hpp
+++ task-08/cxx/core/inc/item-reader/stub-item-reader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+#include <utility>
 #include <core/inc/types.hpp>
 
 #include <core/inc/item-reader/item-reader-iface.hpp>
@@ -16,6 +18,14 @@ struct StubItemReader: ItemReader
 
     ItemInfo read_stats(ItemIdx idx) const override;
     ItemData read_chunk(ItemIdx idx) override;
+
+private:
+
+    using Dataset = std::unordered_map<ItemIdx, std::pair<ItemData, ItemInfo>>;
+    Dataset m_dataset;
+
+    ItemList m_item_list;
+    DataSize m_chunk_size;
 
 };
 
--- task-08/cxx/core/src/item-reader/stub-item-reader.cpp
+++ task-08/cxx/core/src/item-reader/stub-item-reader.cpp
@@ -1,6 +1,9 @@
 #include <core/inc/item-reader/stub-item-reader.hpp>
 
+#include <algorithm>
+#include <cassert>
 #include <memory>
+#include <stdexcept>
 #include <core/inc/types.hpp>
 
 #include <core/inc/item-reader/item-reader-iface.hpp>
@@ -12,31 +15,67 @@ auto StubItemReader::create(const DataStub& data_set, const DataSize& chunk_size
     return std::make_unique<StubItemReader>(data_set, chunk_size);
 }
 
-StubItemReader::StubItemReader(DataStub data_set, DataSize chunk_size)
+StubItemReader::StubItemReader(DataStub data_set, DataSize chunk_size): m_chunk_size(chunk_size)
 {
-    static_cast<void>(data_set);
-    static_cast<void>(chunk_size);
+    if (data_set.size() < 2 or chunk_size == 0)
+    {
+        throw std::length_error{std::string{}};
+    }
+
+    m_dataset.reserve(data_set.size());
+    m_item_list.reserve(data_set.size());
+
+    auto idx = ItemIdx{0};
+    for (const auto& data: data_set)
+    {
+        m_dataset.insert({idx, {data, {.orig_size = data.size(), .rest_size = data.size()}}});
+        m_item_list.push_back(idx);
+
+        ++idx;
+    }
+
+    assert(data_set.size() == m_dataset.size());
+    assert(data_set.size() == m_item_list.size());
 }
 
 auto StubItemReader::enumerate() -> const ItemList&
 {
-    static auto stub = ItemList{};
-
-    return stub;
+    return m_item_list;
 }
 
 auto StubItemReader::read_stats(const ItemIdx idx) const -> ItemInfo
 {
-    static_cast<void>(idx);
-
-    return ItemInfo{};
+    const auto& [data, info] = m_dataset.at(idx);
+    return info;
 }
 
 auto StubItemReader::read_chunk(const ItemIdx idx) -> ItemData
 {
-    static_cast<void>(idx);
+    auto& [data, info] = m_dataset.at(idx);
 
-    return ItemData{};
+    if (info.rest_size == 0)
+    {
+        throw std::underflow_error{std::string{}};
+    }
+
+    assert(info.rest_size > 0);
+    assert(info.rest_size <= info.orig_size);
+
+    const auto idx_begin = info.orig_size - info.rest_size;
+    const auto idx_end   = std::min(idx_begin + m_chunk_size, info.orig_size);
+
+    assert(idx_end > idx_begin);
+    assert(idx_end - idx_begin <= m_chunk_size);
+
+    auto chunk = ItemData(idx_end - idx_begin);
+    std::copy(data.cbegin() + static_cast<decltype(data.cbegin())::difference_type>(idx_begin),
+              data.cbegin() + static_cast<decltype(data.cbegin())::difference_type>(idx_end),
+              chunk.begin());
+
+    assert(chunk.size() <= info.rest_size);
+    info.rest_size -= chunk.size();
+
+    return chunk;
 }
 
 }
