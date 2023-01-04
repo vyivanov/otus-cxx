afb0515225cea122fd62da30205ba7593b500f42
Vladimir Yu. Ivanov <vladimir.u.ivanov@gmail.com>
2023-01-04 16:29:53 +1000

    tmp

--- task-13/cxx/main.cpp
+++ task-13/cxx/main.cpp
@@ -9,20 +9,114 @@
 
 #define PRINT_PARAMETERS 1
 
-struct LogisticRegression {
+namespace Tools {
+
+template<typename Type, typename Tag>
+class FlatBuffer final {
+    static_assert(std::is_arithmetic<Type>::value);
+    static_assert(std::is_class<Tag>::value);
+public:
+    explicit FlatBuffer(const size_t n) {
+        assert(n);
+        m_buffer.resize(n, Type{});
+    }
+    [[nodiscard]]
+    bool is_empty() const noexcept {
+        return size() == 0;
+    }
+    [[nodiscard]]
+    size_t size() const noexcept {
+        return m_buffer.size();
+    }
+    [[nodiscard]]
+    Type& at(const size_t idx) noexcept {
+        assert(idx < size());
+        return m_buffer[idx];
+    }
+    [[nodiscard]]
+    const Type& at(const size_t idx) const noexcept {
+        assert(idx < size());
+        return m_buffer[idx];
+    }
+    [[nodiscard]]
+    Type* ptr() noexcept {
+        return m_buffer.data();
+    }
+    [[nodiscard]]
+    const Type* ptr() const noexcept {
+        return m_buffer.data();
+    }
+private:
+    std::vector<Type> m_buffer = {};
+};
+
+template<typename Type, typename Tag>
+class SquareBuffer final {
+    static_assert(std::is_arithmetic<Type>::value);
+    static_assert(std::is_class<Tag>::value);
+public:
+    SquareBuffer(const size_t row_n, const size_t col_n): m_row_n(row_n), m_col_n(col_n) {
+        assert(m_row_n and m_col_n);
+        m_buffer.resize((m_row_n * m_col_n), Type{});
+    }
+    [[nodiscard]]
+    bool is_empty() const noexcept {
+        return size() == 0;
+    }
+    [[nodiscard]]
+    size_t size() const noexcept {
+        return m_buffer.size();
+    }
+    [[nodiscard]]
+    size_t rows() const noexcept {
+        return m_row_n;
+    }
+    [[nodiscard]]
+    size_t cols() const noexcept {
+        return m_col_n;
+    }
+    [[nodiscard]]
+    Type& at(const size_t row, const size_t col) noexcept {
+        return m_buffer[to_idx(row, col)];
+    }
+    [[nodiscard]]
+    const Type& at(const size_t row, const size_t col) const noexcept {
+        return m_buffer[to_idx(row, col)];
+    }
+    [[nodiscard]]
+    Type* ptr() noexcept {
+        return m_buffer.data();
+    }
+    [[nodiscard]]
+    const Type* ptr() const noexcept {
+        return m_buffer.data();
+    }
+private:
+    [[nodiscard]]
+    size_t to_idx(const size_t row, const size_t col) const noexcept {
+        assert((row < m_row_n) and (col < m_col_n));
+        return (row * m_col_n + col);
+    }
+    const size_t m_row_n = {};
+    const size_t m_col_n = {};
+    std::vector<Type> m_buffer = {};
+};
+
+}
+
+class LogisticRegression {
+public:
     using CoeffType = float;
     using ClassType = int;
 
-    using Weight = std::vector<std::vector<CoeffType>>;
-    using Intercept = std::vector<CoeffType>;
-
-    using Factor = std::vector<CoeffType>;
-    using Proba = std::vector<CoeffType>;
-    using Class = std::vector<ClassType>;
+    using Factors = Tools::SquareBuffer<CoeffType, class WeightsTag>;
+    using Probabs = Tools::SquareBuffer<CoeffType, class WeightsTag>;
+    using Classes = Tools::FlatBuffer<ClassType, class ClassesTag>;
 
     ////////////////
 
-    using Buffer = std::vector<CoeffType>;
+    using Weights = Tools::SquareBuffer<CoeffType, class WeightsTag>;
+    using Intercepts = Tools::FlatBuffer<CoeffType, class InterceptsTag>;
 
     template<typename T>
     using ColVector = Eigen::Matrix<T, Eigen::Dynamic, 1, Eigen::ColMajor>;
@@ -51,133 +145,83 @@ struct LogisticRegression {
     template<typename T>
     using ConstMatrixMap = Eigen::Map<const Matrix<T>>;
 
-    LogisticRegression(const Weight& coeffs, const Intercept& biases)
-        : m_models_num(detect_model_amount(coeffs))
-        , m_features_num(detect_feature_amount(coeffs))
-        , m_coeffs_mtx(to_eigen_matrix(coeffs).transpose())
+    LogisticRegression(const Weights& coeffs, const Intercepts& biases)
+        : m_models_n(coeffs.rows())
+        , m_features_n(coeffs.cols())
+        , m_coeffs_mat(to_eigen_matrix(coeffs).transpose())
         , m_biases_vec(to_eigen_vector(biases).transpose())
     {
-        assert(m_coeffs_mtx.size() and m_biases_vec.size());
+        assert(not coeffs.is_empty());
+        assert(not biases.is_empty());
 
-        assert(m_coeffs_mtx.rows() == m_features_num);
-        assert(m_coeffs_mtx.cols() == m_models_num);
+        assert(m_coeffs_mat.size());
+        assert(m_biases_vec.size());
+
+        assert(m_coeffs_mat.rows() == m_features_n);
+        assert(m_coeffs_mat.cols() == m_models_n);
 
         assert(m_biases_vec.rows() == 1);
-        assert(m_biases_vec.cols() == m_models_num);
+        assert(m_biases_vec.cols() == m_models_n);
 
 #if (PRINT_PARAMETERS == 1)
-        std::cout << "[ models_num ]\n"   << m_models_num   << "\n\n";
-        std::cout << "[ features_num ]\n" << m_features_num << "\n\n";
+        std::cout << "[ models_num ]\n"   << m_models_n   << "\n\n";
+        std::cout << "[ features_num ]\n" << m_features_n << "\n\n";
 
-        std::cout << "[ coeffs ]\n" << m_coeffs_mtx.transpose() << "\n\n";
+        std::cout << "[ coeffs ]\n" << m_coeffs_mat.transpose() << "\n\n";
         std::cout << "[ biases ]\n" << m_biases_vec.transpose() << "\n\n";
 #endif
     }
 
-    void predict(const Factor& samples, Proba& probabs, Class& classes) const {
+    void predict(const Factors& samples, Probabs& probabs, Classes& classes) const {
         predict_proba(samples, probabs);
+        assert(probabs.rows() == samples.rows());
+        assert(probabs.cols() == m_models_n);
 
-        const auto samples_num = detect_samples_num(samples);
-        assert(classes.capacity() == samples_num);
-
-        const auto probabs_mtx = ConstMatrixMap<CoeffType>(probabs.data(), samples_num, m_models_num);
-        auto classes_vec = ColVectorMap<ClassType>(classes.data(), samples_num);
-
-        assert(probabs_mtx.rows() == samples_num);
-        assert(probabs_mtx.cols() == m_models_num);
+        assert(classes.size() == probabs.rows());
+        const auto probabs_mat = ConstMatrixMap<CoeffType>(probabs.ptr(), probabs.rows(), probabs.cols());
+        auto classes_vec = ColVectorMap<ClassType>(classes.ptr(), classes.size());
 
         auto class_idx = ClassType{};
-        for (auto i{0u}; i < samples_num; ++i) {
-            if (m_models_num > 1) {
-                probabs_mtx.row(i).maxCoeff(&class_idx);
+        for (auto i{0u}; i < probabs_mat.rows(); ++i) {
+            if (m_models_n > 1) {
+                probabs_mat.row(i).maxCoeff(&class_idx);
             } else {
-                class_idx = (probabs_mtx(i, 0) < CoeffType{0.5}) ? ClassType{0} : ClassType{1};
+                class_idx = (probabs_mat(i, 0) < CoeffType{0.5}) ? ClassType{0} : ClassType{1};
             }
             classes_vec(i, 0) = class_idx;
         }
     }
 
-    void predict_proba(const Factor& samples, Proba& probabs) const {
-        assert(not (samples.empty()));
+    void predict_proba(const Factors& samples, Probabs& probabs) const {
+        assert(not samples.is_empty());
+        assert(not probabs.is_empty());
 
-        const auto samples_num = detect_samples_num(samples);
-        assert(probabs.capacity() == (samples_num * m_models_num));
+        assert(samples.rows() == probabs.rows());
 
-        const auto samples_mtx = ConstMatrixMap<CoeffType>(samples.data(), samples_num, m_features_num);
-        auto probabs_mtx = MatrixMap<CoeffType>(probabs.data(), samples_num, m_models_num);
+        assert(samples.cols() == m_features_n);
+        assert(probabs.cols() == m_models_n);
 
-        probabs_mtx = (samples_mtx * m_coeffs_mtx).rowwise() + m_biases_vec;
-        sigmoid(probabs_mtx);
+        const auto samples_mat = ConstMatrixMap<CoeffType>(samples.ptr(), samples.rows(), samples.cols());
+        auto probabs_mat = MatrixMap<CoeffType>(probabs.ptr(), probabs.rows(), probabs.cols());
 
-        if (m_models_num > 1) {
-            softmax(probabs_mtx);
+        probabs_mat = (samples_mat * m_coeffs_mat).rowwise() + m_biases_vec;
+        sigmoid(probabs_mat);
+
+        if (m_models_n > 1) {
+            softmax(probabs_mat);
         }
     }
 
 private:
 
     [[nodiscard]]
-    static size_t detect_model_amount(const Weight& coeffs) {
-        return coeffs.size();
+    static Matrix<CoeffType> to_eigen_matrix(const Weights& coeffs) {
+        return ConstMatrixMap<CoeffType>(coeffs.ptr(), coeffs.rows(), coeffs.cols());
     }
 
     [[nodiscard]]
-    static size_t detect_feature_amount(const Weight& coeffs) {
-        if (coeffs.empty()) {
-            return 0;
-        }
-        return coeffs.at(0).size();
-    }
-
-    [[nodiscard]]
-    size_t detect_samples_num(const Factor& samples) const {
-        if (samples.empty()) {
-            return 0;
-        }
-
-        assert(m_features_num);
-        assert(not (samples.size() % m_features_num));
-
-        return (samples.size() / m_features_num);
-    }
-
-    [[nodiscard]]
-    Matrix<CoeffType> to_eigen_matrix(const Weight& coeffs) const {
-        assert(m_models_num and m_features_num);
-
-        const auto coeffs_buf = to_flatten(coeffs);
-        const auto coeffs_mtx = ConstMatrixMap<CoeffType>(coeffs_buf.data(), m_models_num, m_features_num);
-
-        assert(coeffs_mtx.rows() == m_models_num);
-        assert(coeffs_mtx.cols() == m_features_num);
-
-        return coeffs_mtx;
-    }
-
-    [[nodiscard]]
-    ColVector<CoeffType> to_eigen_vector(const Intercept& biases) const {
-        assert(m_models_num);
-
-        const auto biases_vec = ConstColVectorMap<CoeffType>(biases.data(), m_models_num);
-        assert(biases_vec.rows() == m_models_num);
-
-        return biases_vec;
-    }
-
-    [[nodiscard]]
-    Buffer to_flatten(const Weight& coeffs) const {
-        assert(m_models_num and m_features_num);
-        auto buf = Buffer(m_models_num * m_features_num);
-
-        assert(coeffs.size() == m_models_num);
-        for (auto row{0u}; row < m_models_num; ++row) {
-            assert(coeffs.at(row).size() == m_features_num);
-            for (auto col{0u}; col < m_features_num; ++col) {
-                buf.at(row * m_features_num + col) = coeffs.at(row).at(col);
-            }
-        }
-
-        return buf;
+    static ColVector<CoeffType> to_eigen_vector(const Intercepts& biases) {
+        return ConstColVectorMap<CoeffType>(biases.ptr(), biases.size());
     }
 
     static void sigmoid(MatrixMap<CoeffType>& logits) {
@@ -188,85 +232,87 @@ private:
         logits = logits.array().exp().colwise() / logits.array().exp().rowwise().sum();
     }
 
-    const size_t m_models_num{};
-    const size_t m_features_num{};
+    const size_t m_models_n;
+    const size_t m_features_n;
 
-    const Matrix<CoeffType>    m_coeffs_mtx{};
-    const RowVector<CoeffType> m_biases_vec{};
+    const Matrix<CoeffType>    m_coeffs_mat;
+    const RowVector<CoeffType> m_biases_vec;
 
 };
 
 int main() {
-    const auto parse_file = [](const std::filesystem::path& path, const char delim) {
+    const auto parse_file = [](const std::filesystem::path& path, const char delim, size_t row_n, size_t col_n) {
         assert(std::filesystem::exists(path));
         auto stream = std::ifstream{path};
 
-        auto first_column = std::vector<LogisticRegression::CoeffType>{};
-        auto rest_columns = std::vector<std::vector<LogisticRegression::CoeffType>>{};
+        auto first_column = LogisticRegression::Intercepts{row_n};
+        auto rest_columns = LogisticRegression::Factors{row_n, col_n};
 
         auto line_str = std::string{};
         auto line_ss = std::istringstream{};
         auto value_str = std::string{};
 
+        auto row{0u};
+        auto col{0u};
         while (std::getline(stream, line_str)) {
             line_ss.str(line_str);
             std::getline(line_ss, value_str, delim);
-            first_column.push_back(std::strtof(value_str.c_str(), nullptr));
-            auto line = std::vector<LogisticRegression::CoeffType>{};
+            first_column.at(row) = std::strtof(value_str.c_str(), nullptr);
             while (std::getline(line_ss, value_str, delim)) {
-                line.push_back(std::strtof(value_str.c_str(), nullptr));
+                rest_columns.at(row, col) = std::strtof(value_str.c_str(), nullptr);
+                ++col;
             }
-            rest_columns.push_back(line);
             line_ss = std::istringstream{};
+            ++row;
+            col = 0;
         }
 
         return std::make_pair(first_column, rest_columns);
     };
 
-    const auto [biases, coeffs] = parse_file("../../ml-artifacts/12_CV/logreg_coef.txt", '\x20');
+    const auto [biases, coeffs] = parse_file("../../ml-artifacts/12_CV/logreg_coef.txt", '\x20', 10, 784);
     assert(biases.size() == 10);
-    assert(coeffs.size() == 10 and coeffs.at(0).size() == 784);
+    assert(coeffs.rows() == 10 and coeffs.cols() == 784);
 
-    const auto [classes_true, samples] = parse_file("../../ml-artifacts/12_CV/test.csv", ',');
+    auto [classes_true, samples] = parse_file("../../ml-artifacts/12_CV/test.csv", ',', 12'000, 784);
     assert(classes_true.size() == 12'000);
-    assert(samples.size() == 12'000 and samples.at(0).size() == 784);
+    assert(samples.rows() == 12'000 and samples.cols() == 784);
 
-    auto samples_flat = LogisticRegression::Factor{};
-    for (const auto& row: samples) {
-        for (const auto& col: row) {
-            samples_flat.push_back(col / 255.f);
+    auto probabs_pred = LogisticRegression::Probabs(samples.rows(), coeffs.rows());
+    auto classes_pred = LogisticRegression::Classes(samples.rows());
+
+//    std::cout << LogisticRegression::ConstMatrixMap<float>(samples.ptr(), samples.rows(), samples.cols());
+
+    for (auto row{0u}; row < samples.rows(); ++row) {
+        for (auto col{0u}; col < samples.cols(); ++col) {
+            samples.at(row, col) /= 255.f;
         }
     }
 
-    assert(samples_flat.size() == (samples.size() * samples.at(0).size()));
-
-    auto probabs_pred = LogisticRegression::Proba(samples.size() * coeffs.size());
-    auto classes_pred = LogisticRegression::Class(samples.size());
-
     const auto clf = LogisticRegression{coeffs, biases};
-    clf.predict(samples_flat, probabs_pred, classes_pred);
+    clf.predict(samples, probabs_pred, classes_pred);
 
-    if (samples.size() < 15) {
+    if (samples.rows() < 15) {
         std::cout <<
             ">> Probability distributions :\n" <<
             LogisticRegression::ConstMatrixMap<LogisticRegression::CoeffType>(
-                probabs_pred.data(),
-                samples.size(), coeffs.size()) << "\n\n";
+                probabs_pred.ptr(),
+                samples.rows(), coeffs.rows()) << "\n\n";
 
         std::cout <<
             ">> Predicted classes :\n" <<
             LogisticRegression::ConstColVectorMap<LogisticRegression::ClassType>(
-                classes_pred.data(),
-                samples.size()) << "\n\n";
+                classes_pred.ptr(),
+                samples.rows()) << "\n\n";
     }
 
     auto ok_acc{0u};
-    for (auto idx{0u}; idx < samples.size(); ++idx) {
+    for (auto idx{0u}; idx < samples.rows(); ++idx) {
         if (classes_pred.at(idx) == classes_true.at(idx)) {
             ++ok_acc;
         }
     }
 
     std::cout <<
-        ">> Accuracy :\n" << ok_acc * 100.0f / samples.size() << "\n\n";
+        ">> Accuracy :\n" << ok_acc * 100.0f / samples.rows() << "\n\n";
 }
